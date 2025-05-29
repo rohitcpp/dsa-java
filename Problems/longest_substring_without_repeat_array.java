b74bb116ed0fdd4d6d658c1d7cf285d78e80ffc4
unknown <rohitdmore2255@gmail.com>
2025-05-29 18:15:47 +0530

    figuring out longest_substring_without_repeat_array

---
+++ Problems/longest_substring_without_repeat_array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] lastSeen = new int[128]; // ASCII characters
+        int start = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // If character was seen in current window, move start
+            if (lastSeen[c] > start) {
+                start = lastSeen[c];
+            }
+            // Update max length
+            maxLen = Math.max(maxLen, i - start + 1);
+            // Store position of current character (1-based index)
+            lastSeen[c] = i + 1;
+        }
+
+        return maxLen;
+    }
+}
